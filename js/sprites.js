b6c1852abd5c8f2a3e66466705b8c5826d2722d6
SandarvaShakya <sandarvashakya34@gmail.com>
2023-08-14 08:59:01 +0545

    🛠 Fix Image Path

--- js/sprites.js
+++ js/sprites.js
@@ -1,23 +1,23 @@
 const roadImg = new Image()
-roadImg.src = '/assets/images/road-new.png'
+roadImg.src = 'assets/images/road-new.png'
 roadImg.onload = () => {
     roadLoaded = true
 }
 
 const enemyImg = new Image()
-enemyImg.src = '/assets/images/truck4.png'
+enemyImg.src = 'assets/images/truck4.png'
 enemyImg.onload = () => {
     enemyLoaded = true
 }
 
 const playerImg = new Image()
-playerImg.src = '/assets/images/car1.png'
+playerImg.src = 'assets/images/car1.png'
 playerImg.onload = () => {
     playerLoaded = true
 }
 
 const finishLineImg = new Image()
-finishLineImg.src = '/assets/images/finish.png'
+finishLineImg.src = 'assets/images/finish.png'
 finishLineImg.onload = () => {
     finishLineLoaded = true
 }
